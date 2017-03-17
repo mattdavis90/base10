554b8c23b23b42c2aab8415543b134636099f045
Matt Davis <mattdavis90@googlemail.com>
2017-03-17 23:30:29 +0000

    Fixed MANIFEST.in. Updated to v0.4

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 setup(
     name='base10',
-    packages=['base10'],
-    version='0.3',
+    packages=find_packages('.'),
+    version='0.4',
     license='MIT',
     description='Base10 is a metrics abstraction layer for \
             linking multiple metrics source and stores. It also \
@@ -10,7 +10,20 @@ setup(
     author='Matt Davis',
     author_email='mattdavis90@googlemail.com',
     url='https://github.com/mattdavis90/base10',
-    download_url='https://github.com/mattdavis90/base10/archive/0.3.tar.gz',
+    download_url='https://github.com/mattdavis90/base10/archive/0.4.tar.gz',
     keywords=['metrics', 'abstraction'],
-    classifiers=[],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: System :: Monitoring",
+    ],
 )
